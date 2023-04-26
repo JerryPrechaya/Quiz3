303b3f4af6f4c4cb758601260b28a495b324b6e8
T1 <prechaya.maks@kmutt.ac.th>
2023-04-26 14:55:39 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace workshopweek4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[] market = new char[n];
+            for (int i = 0; i < n; i++)
+            {
+                market[i] = '-';
+            }
+
+            while (true)
+            {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+
+                if (a != 0 || b != 0 || a > 0 || b > 0)
+                {
+                    market[a - 1] = 'X';
+                    market[b - 1] = 'X';
+                }
+                else if (a < 1)
+                {
+                    market[b - 1] = 'X';
+                }
+                else if (b < 1)
+                {
+                    market[a - 1] = 'X';
+                }
+                else if (a == b)
+                {
+                    market[a - 1] = 'X';
+                }
+                else if (a < 1 && b < 1)
+                {
+                    break;
+                }
+                break;
+            }
+
+            for (int k = 0; k < n; k++)
+            {
+                Console.Write(market[k]);
+            }
+
+        }
+
+    }
+}
+
+
